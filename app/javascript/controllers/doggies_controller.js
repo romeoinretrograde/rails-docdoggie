106c85cdbe0a5a7d25dff7864046e064e2a66e57
Danilo Guimaraes <guimaraes.louie@gmail.com>
2022-11-30 14:46:20 +0000

    Doggies new is working

--- app/javascript/controllers/doggies_controller.js
+++ app/javascript/controllers/doggies_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="doggies"
 export default class extends Controller {
-  connect() {
-    console.log("Woof woof!");
+
+  static targets = ["form", "input"]
+
+  getImage(event) {
+    event.preventDefault()
+    const dog = document.querySelector(".active")
+    const image = `doggies/${dog.firstElementChild.id}.png`
+    this.inputTarget.value = image
+    this.formTarget.submit()
   }
 }

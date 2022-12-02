9635a0da1893c64997a1ba1cc6321f6bdf83523a
Danilo Guimaraes <guimaraes.louie@gmail.com>
2022-12-02 15:14:00 +0000

    fixed some stuff and the user query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(user)
+    if user.doggie.nil?
+      new_doggy_path(user)
+    else
+      dashboard_path(user)
+    end
+  end
 end
